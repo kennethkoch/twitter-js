c8c95ee84532a773e80e1424e403ace3f190ba36
Sandy Mak <sandy.mak91@gmail.com>
2017-11-07 15:34:23 -0500

    more work

--- app.js
+++ app.js
@@ -2,46 +2,51 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const PORT = 3000;
-const nunjucks = require('nunjucks');
+const routes = require('./routes');
 
+
+app.use('/', routes);
+
+const nunjucks = require('nunjucks');
+const tweetBank = require('./tweetBank');
 
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
 })
 
-// morgan(function (tokens, req, res) {
-//  return [
-//    tokens.method(req, res),
-//    tokens.url(req, res),
-//    tokens.status(req, res),
-//    tokens.res(req, res, 'content-length'), '-',
-//    tokens['response-time'](req, res), 'ms'
-//  ].join(' ')
-// })
+// // morgan(function (tokens, req, res) {
+// //  return [
+// //    tokens.method(req, res),
+// //    tokens.url(req, res),
+// //    tokens.status(req, res),
+// //    tokens.res(req, res, 'content-length'), '-',
+// //    tokens['response-time'](req, res), 'ms'
+// //  ].join(' ')
+// // })
 
 app.use(morgan('dev'));
 
-// app.use(function (req, res, next) {
-//   console.log(req.method + ' ' + req.originalUrl + ' ' + res.statusCode);
-// })
+app.use(function (req, res, next) {
+  console.log(req.method + ' ' + req.originalUrl + ' ' + res.statusCode);
+})
 
-app.get('/news', (req, res) => {
+// app.get('/news', (req, res) => {
 
-});
+// });
 
-var locals = {
-  title: 'An Example',
-  people: [{
-      name: 'Gandalf'
-    },
-    {
-      name: 'Frodo'
-    },
-    {
-      name: 'Hermione'
-    }
-  ]
-};
+// var locals = {
+//   title: 'An Example',
+//   people: [{
+//       name: 'Gandalf'
+//     },
+//     {
+//       name: 'Frodo'
+//     },
+//     {
+//       name: 'Hermione'
+//     }
+//   ]
+// };
 
 app.set('view engine', 'html');
 app.engine('html', nunjucks.render);
@@ -50,8 +55,26 @@ nunjucks.configure('views', {
   noCache: true
 });
 
-app.get('/views', (req, res) => {
-  nunjucks.render('index.html', locals, function (err, output) {
-    res.send(output);
-  });
-});
+// // Different way to render HTML
+// // app.get('/views', (req, res) => {
+// //   nunjucks.render('index.html', locals, function (err, output) {
+// //     res.send(output);
+// //     // console.log(output)
+// //   });
+// // });
+
+// app.get('/views', (req, res) => {
+//   const people = [{
+//     name: 'Full'
+//   }, {
+//     name: 'Stacker'
+//   }, {
+//     name: 'Son'
+//   }];
+//   res.render('index', {
+//     title: 'Hall of Fame',
+//     people: people
+//   });
+// });
+
+// console.log(tweetBank.find());
