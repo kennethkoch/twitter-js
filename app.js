67a7789d76585d81578975fd2b75ce45fd002593
Kenny Koch <kennykoch47@gmail.com>
2017-11-07 12:03:22 -0500

    updated app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express();
+const morgan = require('morgan')
 
 const PORT = 3000;
 
@@ -7,7 +8,21 @@ app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
 })
 
+// morgan(function (tokens, req, res) {
+//  return [
+//    tokens.method(req, res),
+//    tokens.url(req, res),
+//    tokens.status(req, res),
+//    tokens.res(req, res, 'content-length'), '-',
+//    tokens['response-time'](req, res), 'ms'
+//  ].join(' ')
+// })
 
+app.use(morgan('dev'))
+
+// app.use(function (req, res, next) {
+//   console.log(req.method + ' ' + req.originalUrl + ' ' + res.statusCode);
+// })
 
 app.get('/news', (req, res) => {
 
